cac4f111cc05206764007eb0ad440f45a059a2e5
artsiomshshshsk <artsiomshablinskiy@gmail.com>
2023-04-07 13:55:39 +0200

    initial commit

---
+++ src/main/java/org/task1/TreeNode.java
@@ -0,0 +1,20 @@
+package org.task1;
+
+import lombok.Getter;
+
+@Getter
+public class TreeNode {
+    private int val;
+    private TreeNode left;
+    private TreeNode right;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
---
+++ src/main/java/org/task1/TreeService.java
@@ -0,0 +1,51 @@
+package org.task1;
+
+public class TreeService {
+
+
+
+    /**
+     * Calculate number of all leaves (nodes without children)
+     *
+     * @param node root node of subtree
+     * @return number of all leaves
+     */
+    public int countLeaves(TreeNode node){
+        if(node == null) return 0;
+        if(node.getLeft() == null && node.getRight() == null) return 1;
+        return countLeaves(node.getLeft()) + countLeaves(node.getRight()) +
+                (node.getLeft() == null && node.getRight() == null ? 1 : 0);
+    }
+
+
+    /**
+     * Calculate the largest number of edges in a path
+     * from the root node to a leaf node
+     *
+     * @param node root node of subtree
+     * @return median of nodes
+     */
+    public int longestPath(TreeNode node){
+        if(node == null) return 0;
+
+        if(node.getLeft() == null && node.getRight() == null){
+            return 0;
+        }
+        return 1 + Math.max(longestPath(node.getLeft()), longestPath(node.getRight()));
+    }
+
+
+    /**
+     * Check two trees for equality
+     *
+     * @param tree1 root node of the first tree
+     * @param tree2 root node of the second tree
+     * @return boolean - result of equality check
+     */
+    public boolean checkForEquality(TreeNode tree1, TreeNode tree2){
+        if(tree1 == null && tree2 == null) return true;
+        if(tree1 == null || tree2 == null) return false;
+        if(tree1.getVal() != tree2.getVal()) return false;
+        return checkForEquality(tree1.getLeft(),tree2.getLeft()) && checkForEquality(tree1.getRight(),tree2.getRight());
+    }
+}
