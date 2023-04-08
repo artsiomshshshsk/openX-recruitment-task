fca1edcde0eeb7ad8114e3b45e530acc66932eaa
artsiomshshshsk <artsiomshablinskiy@gmail.com>
2023-04-08 12:21:21 +0200

    added tests for longest path

--- src/test/java/com/github/artsiomshshshsk/TreeServiceTest.java
+++ src/test/java/com/github/artsiomshshshsk/TreeServiceTest.java
@@ -42,4 +42,67 @@ class TreeServiceTest {
         assertEquals(0, count);
     }
 
+    @Test
+    void testLongestPath() {
+        /*
+         * Test tree:
+         *        10
+         *       /  \
+         *      20  30
+         *     / \   / \
+         *    40 50 60 70
+         */
+        TreeNode root = new TreeNode(10,
+                new TreeNode(20,
+                        new TreeNode(40),
+                        new TreeNode(50)),
+                new TreeNode(30,
+                        new TreeNode(60),
+                        new TreeNode(70))
+        );
+
+        TreeService treeService = new TreeService();
+        int longestPath = treeService.longestPath(root);
+        assertEquals(2, longestPath);
+    }
+
+    @Test
+    void testLongestPathNullNode() {
+        TreeService treeService = new TreeService();
+        int longestPath = treeService.longestPath(null);
+
+        assertEquals(0, longestPath);
+    }
+
+    @Test
+    void testLongestPathSingleNode() {
+        TreeNode node = new TreeNode(1);
+        TreeService treeService = new TreeService();
+        int longestPath = treeService.longestPath(node);
+        assertEquals(0, longestPath);
+    }
+
+    @Test
+    void testLongestPathTwoLeaves() {
+        /*
+         * Test tree:
+         *      1
+         *     / \
+         *    2   3
+         *       / \
+         *      4   5
+         */
+        TreeNode root = new TreeNode(1,
+                new TreeNode(2),
+                new TreeNode(3,
+                        new TreeNode(4),
+                        new TreeNode(5))
+        );
+        TreeService treeService = new TreeService();
+        int longestPath = treeService.longestPath(root);
+        assertEquals(2, longestPath);
+    }
+
+
+
 }
