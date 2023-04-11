9d88a5ecf948154b59c730fe001716f33d64383f
artsiomshshshsk <artsiomshablinskiy@gmail.com>
2023-04-11 14:49:05 +0200

    json property to address

--- src/main/java/com/github/artsiomshshshsk/shopping/model/Address.java
+++ src/main/java/com/github/artsiomshshshsk/shopping/model/Address.java
@@ -1,6 +1,7 @@
 package com.github.artsiomshshshsk.shopping.model;
 
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
@@ -9,5 +10,7 @@ public class Address {
     private String city;
     private String street;
     private int number;
+
+    @JsonProperty("zipcode")
     private String zipCode;
 }
