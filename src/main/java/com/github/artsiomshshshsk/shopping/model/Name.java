274b7ea0f7ea09e5371eb53b810fa5e63c488789
artsiomshshshsk <artsiomshablinskiy@gmail.com>
2023-04-11 14:49:42 +0200

    json property to name class

--- src/main/java/com/github/artsiomshshshsk/shopping/model/Name.java
+++ src/main/java/com/github/artsiomshshshsk/shopping/model/Name.java
@@ -1,10 +1,14 @@
 package com.github.artsiomshshshsk.shopping.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class Name {
 
+    @JsonProperty("firstname")
     private String firstName;
+
+    @JsonProperty("lastname")
     private String lastName;
 }
